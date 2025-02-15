197374985a7f1b0513db64c99728b941cee624df
sai2327 <saitejak2327@gmail.com>
2025-02-15 13:08:34 +0530

    Update Insertionsort.c

--- Insertionsort.c
+++ Insertionsort.c
@@ -1,17 +1,14 @@
 #include<stdio.h>
-void Insertion_sort(int A[],int n){
-	int i,j;
-	for(i=0;i<n-1;i++){
-		for(j=0;j<=i;j++){
-			int con = A[i+1];
-			//select the element using con 
-			if(A[j]>con){
-			//if con is less the its preveious number it will swap
-				int temp = A[j];
-				A[j] = A[i+1];
-				A[i+1] = temp;
-			}
+void Insertion_sort(int arr[],int n){
+	for(int i=1;i<n;i++){
+		//unsorted array lo first element is i which is stored in temp
+		int temp=arr[i];
+		int j=i-1; //sorted array lo last element j-1
+		while(j>=0 && temp<arr[j]){ //checking if temp if less than the elements present in sorted part
+			arr[j+1]=arr[j]; //moving the part to forward 
+			j--;
 		}
+		arr[j+1]=temp; //placing the element at the correct position
 	}
 }
 int main(){
